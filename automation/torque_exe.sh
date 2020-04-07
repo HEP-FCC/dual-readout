60bc589cc96052ca903e78fb1db28c6c1b716160
Kyuyeong Hwang <kyuyeong.hwang@cern.ch>
2020-04-07 12:29:09 +0900

    Removing automation scripts.

--- automation/torque_exe.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-source /path/to/build/Torque_env.sh
-cd /path/to/build/0
-
-/path/to/build/DRsim /path/to/build/run_calib.mac $1
--- automation/torque_sub.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-for i in {1..500}
-do
-    qsub -q large -o /path/to/build/0/log -e /path/to/build/0/log -F "$i" /path/to/build/torque_exe.sh
-    echo job $i submitted
-done
