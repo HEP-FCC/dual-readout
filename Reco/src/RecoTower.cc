c6e448bcf1fbd2a80ffe0ed74d2bbbca06aab87f
Sanghyun Ko <sang.hyun.ko@cern.ch>
2020-06-08 21:27:57 +0900

    remove tower jets and change R to 0.8

--- Reco/src/RecoTower.cc
+++ Reco/src/RecoTower.cc
@@ -59,38 +59,12 @@ void RecoTower::reconstruct(const DRsimInterface::DRsimTowerData& tower, RecoInt
   recoTower.E_DR = E_DR(recoTower.E_C,recoTower.E_S);
   recoTower.E_DRcorr = E_DR(recoTower.E_C,recoTower.E_Scorr);
 
-  addFjInputs(recoTower);
   fData = recoTower;
   evt.towers.push_back(fData);
 }
 
 int RecoTower::getAbsITheta(const int iTheta) {
   return iTheta >= 0 ? iTheta : -iTheta -1;
-}
-
-void RecoTower::addFjInputs(const RecoInterface::RecoTowerData& recoTower) {
-  TVector3 p_DR;
-  p_DR.SetMagThetaPhi(recoTower.E_DR,recoTower.theta.second,recoTower.phi.second);
-  fFjInputs_DR.push_back( fastjet::PseudoJet(p_DR.x(),p_DR.y(),p_DR.z(),recoTower.E_DR) );
-
-  TVector3 p_DRcorr;
-  p_DRcorr.SetMagThetaPhi(recoTower.E_DRcorr,recoTower.theta.second,recoTower.phi.second);
-  fFjInputs_DRcorr.push_back( fastjet::PseudoJet(p_DRcorr.x(),p_DRcorr.y(),p_DRcorr.z(),recoTower.E_DRcorr) );
-
-  TVector3 p_S;
-  p_S.SetMagThetaPhi(recoTower.E_S,recoTower.theta.second,recoTower.phi.second);
-  fFjInputs_S.push_back( fastjet::PseudoJet(p_S.x(),p_S.y(),p_S.z(),recoTower.E_S) );
-
-  TVector3 p_Scorr;
-  p_Scorr.SetMagThetaPhi(recoTower.E_Scorr,recoTower.theta.second,recoTower.phi.second);
-  fFjInputs_Scorr.push_back( fastjet::PseudoJet(p_Scorr.x(),p_Scorr.y(),p_Scorr.z(),recoTower.E_Scorr) );
-}
-
-void RecoTower::clear() {
-  fFjInputs_S.clear();
-  fFjInputs_Scorr.clear();
-  fFjInputs_DR.clear();
-  fFjInputs_DRcorr.clear();
 }
 
 float RecoTower::E_DR(float E_C, float E_S) {
