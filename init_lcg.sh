16357755b334ea6b6bfef52fb4c16ed3aa7c782d
Valentin Volkl <valentin.volkl@cern.ch>
2020-09-14 10:07:07 +0200

    [ci] add github actions
    add ci config
    
    up
    
    [ci] use lcg view for now
    
    [ci] use lcg view for now
    
    [ci] fixes
    
    up

---
+++ init_lcg.sh
@@ -0,0 +1,5 @@
+
+source /cvmfs/sft.cern.ch/lcg/views/LCG_97_FCC_2/x86_64-centos7-gcc8-opt/setup.sh
+export PYTHIA8_ROOT_DIR=/cvmfs/sft.cern.ch/lcg/views/LCG_97_FCC_2/x86_64-centos7-gcc8-opt/
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_97a_FCC_2/hepmc3/3.2.2/x86_64-centos7-gcc8-opt/hepmc3-env.sh
+
