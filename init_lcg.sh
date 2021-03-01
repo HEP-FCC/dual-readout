b2d43e7657462878f62bd9a043d809f22b3a4e77
Sanghyun Ko <sang.hyun.ko@cern.ch>
2021-03-01 14:30:01 +0900

    downgrade dd4hep to fix undefined GeoSvc links

--- init_lcg.sh
+++ init_lcg.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_4/x86_64-centos7-gcc8-opt/setup.sh
-export PYTHIA8_ROOT_DIR=/cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_4/x86_64-centos7-gcc8-opt/
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_97a_FCC_4/hepmc3/3.2.2/x86_64-centos7-gcc8-opt/hepmc3-env.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_1/x86_64-centos7-gcc8-opt/setup.sh
+export PYTHIA8_ROOT_DIR=/cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_1/x86_64-centos7-gcc8-opt/
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_97a_FCC_1/hepmc3/3.2.2/x86_64-centos7-gcc8-opt/hepmc3-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_97a_FCC_1/DD4hep/01-12-01/x86_64-centos7-gcc8-opt/bin/thisdd4hep.sh
