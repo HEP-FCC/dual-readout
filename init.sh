b5e1f5e0baaea561fef31d9cb7e14b0dd5660952
Sanghyun Ko <sang.hyun.ko@cern.ch>
2021-05-13 17:27:54 +0900

    [WIP] migrate reconstruction to K4FWCore (2) - CMake fix

--- init.sh
+++ init.sh
@@ -1,1 +1,4 @@
-source /cvmfs/sw.hsf.org/spackages/edm4hep.sh
+#!/bin/sh
+
+#source /cvmfs/sw.hsf.org/key4hep/setup.sh
+source /cvmfs/sw.hsf.org/spackages2/key4hep-stack/2021-04-30/x86_64-centos7-gcc8.3.0-opt/t5gcd6ltt2ikybap2ndoztsg5uyorxzg/setup.sh
--- init_lcg.sh
+++ init_lcg.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_100rc2/x86_64-centos7-gcc8-opt/setup.sh
-export PYTHIA8_ROOT_DIR=/cvmfs/sft.cern.ch/lcg/views/LCG_100rc2/x86_64-centos7-gcc8-opt/
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_100rc2/hepmc3/3.2.3/x86_64-centos7-gcc8-opt/hepmc3-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_100rc2/DD4hep/01.16.01/x86_64-centos7-gcc8-opt/bin/thisdd4hep.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc8-opt/setup.sh
+export PYTHIA8_ROOT_DIR=/cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc8-opt/
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_100/hepmc3/3.2.3/x86_64-centos7-gcc8-opt/hepmc3-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_100/DD4hep/01.16.01/x86_64-centos7-gcc8-opt/bin/thisdd4hep.sh
+
+# LCG100 has no k4FWCore
+# install it manually and do 'export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:<path_to_k4FWCore_install>/lib/cmake/k4FWCore'
+# HSF spackages do not work on ivy-bridge architecture
