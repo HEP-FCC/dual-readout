3c5d15fd34983701348450aade720a8a2d322e35
Sanghyun Ko <sang.hyun.ko@cern.ch>
2021-06-19 20:50:17 +0900

    move compact and calib to share, reset calib constants

--- Reco/include/DRcalib2D.h
+++ Reco/include/DRcalib2D.h
@@ -35,7 +35,7 @@ private:
   DataHandle<edm4hep::RawCalorimeterHitCollection> m_digiHits{"DigiCalorimeterHits", Gaudi::DataHandle::Reader, this};
   DataHandle<edm4hep::CalorimeterHitCollection> m_caloHits{"DRcalo2dHits", Gaudi::DataHandle::Writer, this};
 
-  Gaudi::Property<std::string> m_calibPath{this, "calibPath", "calib.csv", "relative path to calibration csv file"};
+  Gaudi::Property<std::string> m_calibPath{this, "calibPath", "share/calib.csv", "relative path to calibration csv file"};
   Gaudi::Property<std::string> m_readoutName{this, "readoutName", "DRcaloSiPMreadout", "readout name of DRcalo"};
 
   Gaudi::Property<double> m_sampling{this, "sampling", 0.1, "SiPM sampling rate in ns"};
