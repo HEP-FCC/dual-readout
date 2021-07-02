83c4eb5149cad8854b3e2ad80c33b51f8a69bf39
Sanghyun Ko <sang.hyun.ko@cern.ch>
2021-07-02 19:42:30 +0900

    setup git actions for hsf env (2)

--- init_hsf.sh
+++ init_hsf.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-#source /cvmfs/sw.hsf.org/key4hep/setup.sh
-source /cvmfs/sw.hsf.org/spackages2/key4hep-stack/2021-04-30/x86_64-centos7-gcc8.3.0-opt/t5gcd6ltt2ikybap2ndoztsg5uyorxzg/setup.sh
+source /cvmfs/sw.hsf.org/key4hep/setup.sh
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source $SCRIPT_DIR/init_sipm.sh
