02dd54c2e748532caf1b44ef7655f968120635bc
Sanghyun Ko <sang.hyun.ko@cern.ch>
2021-07-02 19:40:42 +0900

    setup git actions for hsf env (1)

--- .github/scripts/compile_and_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir build install
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17  .. && \
-make  -j `getconf _NPROCESSORS_ONLN` && \
-make install && \
-ls -alhR $G4LEDATA && \
-ctest -j `getconf _NPROCESSORS_ONLN` --output-on-failure
