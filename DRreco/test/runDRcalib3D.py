067fdf91fedede00c7e318df2e799c9d2900bce6
Sanghyun Ko <sang.hyun.ko@cern.ch>
2021-07-14 21:35:04 +0900

    migrate 3d calibration to key4hep (1)

---
+++ DRreco/test/runDRcalib3D.py
@@ -0,0 +1,49 @@
+from Gaudi.Configuration import *
+from Configurables import ApplicationMgr
+
+from Configurables import k4DataSvc
+dataservice = k4DataSvc("EventDataSvc", input="digi.root")
+
+from Configurables import PodioInput
+podioinput = PodioInput("PodioInput",
+  collections = [
+    "DigiCalorimeterHits",
+    "DigiWaveforms",
+    "RawTimeStructs",
+    "RawCalorimeterHits",
+    "SimCalorimeterHits",
+    "RawWavlenStructs",
+    "GenParticles",
+    "Leakages"
+  ]
+)
+
+from Configurables import GeoSvc
+geoservice = GeoSvc(
+    "GeoSvc",
+    detectors = [
+        'file:share/compact/DRcalo.xml'
+    ]
+)
+
+from Configurables import DRcalib2D
+calib2d = DRcalib2D("DRcalib2D")
+
+from Configurables import DRcalib3D
+calib3d = DRcalib3D("DRcalib3D")
+
+from Configurables import PodioOutput
+podiooutput = PodioOutput("PodioOutput", filename = "reco.root")
+podiooutput.outputCommands = ["keep *"]
+
+ApplicationMgr(
+    TopAlg = [
+        podioinput,
+        calib2d,
+        calib3d,
+        podiooutput
+    ],
+    EvtSel = 'NONE',
+    EvtMax = 10,
+    ExtSvc = [dataservice, geoservice]
+)
