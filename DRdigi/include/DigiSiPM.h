8c410b008089db50188435a796193da9e5bf4edc
EdoPro98 <edoardo.proserpio@gmail.com>
2021-07-15 19:02:57 +0200

    SiPMAdc is still experimental and under discussion. Modified to use only analog signal

--- DRdigi/include/DigiSiPM.h
+++ DRdigi/include/DigiSiPM.h
@@ -11,7 +11,6 @@
 #include "GaudiKernel/ToolHandle.h"
 
 #include "SiPMSensor.h"
-#include "SiPMAdc.h"
 
 class DigiSiPM : public GaudiAlgorithm {
 public:
@@ -30,7 +29,6 @@ private:
   DataHandle<edm4hep::SparseVectorCollection> m_waveforms{"DigiWaveforms", Gaudi::DataHandle::Writer, this};
 
   std::unique_ptr<sipm::SiPMSensor> m_sensor;
-  std::unique_ptr<sipm::SiPMAdc> m_adc;
 
   // Hamamatsu S14160-1310PS
   Gaudi::Property<double> m_sigLength{this, "signalLength", 200., "signal length in ns"};
@@ -41,20 +39,14 @@ private:
   Gaudi::Property<double> m_recovery{this, "recovery", 10., "SiPM cell recovery time in ns"}; // https://arxiv.org/abs/2001.10322
   Gaudi::Property<double> m_cellPitch{this, "cellpitch", 10., "SiPM cell size in um"};
   Gaudi::Property<double> m_afterpulse{this, "afterpulse", 0.03, "afterpulse probability"};
-  Gaudi::Property<double> m_falltimeFast{this, "falltimeFast", 6.5, "signal fast component decay time in ns"};
+  Gaudi::Property<double> m_falltimeFast{this, "falltimeFast", 6.5, "signal fast component decay time in ns"};  // This looks too fast
   Gaudi::Property<double> m_risetime{this, "risetime", 1., "signal rise time in ns"};
   Gaudi::Property<double> m_snr{this, "SNR", 30., "SNR value in dB"};
 
-  // ADC parameters
-  Gaudi::Property<unsigned int> m_bits{this, "bits", 14, "ADC bits"};
-  Gaudi::Property<double> m_range{this, "range", std::pow(2., 13.), "ADC output range"};
-  Gaudi::Property<double> m_gain{this, "gain", 13.979, "ADC gain in dB i.e. gain(linear) = 10^(m_gain/20)"};
-  // reciprocal_width (multiplication factor) = 1. / (range / gain(linear) / 2^bits);
-
   // integration parameters
   Gaudi::Property<double> m_gateStart{this, "gateStart", 10., "Integration gate starting time in ns"};
-  Gaudi::Property<double> m_gateL{this, "gateLength", 90., "Integration gate length in ns"};
-  Gaudi::Property<int> m_thres{this, "threshold", static_cast<int>( 1.5 * 10. ), "Integration threshold"};
+  Gaudi::Property<double> m_gateL{this, "gateLength", 90., "Integration gate length in ns"};  // Should be approx 5 times fallTimeFast (see above)
+  Gaudi::Property<double> m_thres{this, "threshold", 1.5, "Integration threshold"};  // Threshold in pe (1.5 to suppress DCR)
 };
 
 #endif
