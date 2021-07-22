cd9f1beab0676819aca5cec269651340d59baab3
Sanghyun Ko <sang.hyun.ko@cern.ch>
2021-07-22 21:34:39 +0900

    modify DRcalib3D to store 3d hits

--- DRreco/include/DRcalib3D.h
+++ DRreco/include/DRcalib3D.h
@@ -13,8 +13,6 @@
 #include "GridDRcalo.h"
 #include "k4Interface/IGeoSvc.h"
 
-class TString;
-class TH1;
 class IGeoSvc;
 
 class DRcalib3D : public GaudiAlgorithm {
@@ -29,6 +27,10 @@ public:
   TH1* processFFT(TH1* waveHist);
 
 private:
+  void addToTimeStruct(edm4hep::CalorimeterHitCollection* caloHits, const edm4hep::CalorimeterHit& hit2d,
+                       const dd4hep::Position& sipmPos, const dd4hep::Position& fiberUnit,
+                       double invVminusInvC, double integral, double con, double cen);
+
   ServiceHandle<IGeoSvc> m_geoSvc;
   dd4hep::DDSegmentation::GridDRcalo* pSeg;
   dd4hep::DDSegmentation::DRparamBase* pParamBase;
@@ -36,7 +38,7 @@ private:
   DataHandle<edm4hep::RawCalorimeterHitCollection> m_digiHits{"DigiCalorimeterHits", Gaudi::DataHandle::Reader, this};
   DataHandle<edm4hep::SparseVectorCollection> m_waveforms{"DigiWaveforms", Gaudi::DataHandle::Reader, this};
   DataHandle<edm4hep::CalorimeterHitCollection> m_2dHits{"DRcalo2dHits", Gaudi::DataHandle::Reader, this};
-  // DataHandle<edm4hep::CalorimeterHitCollection> m_caloHits{"DRcalo3dHits", Gaudi::DataHandle::Writer, this};
+  DataHandle<edm4hep::CalorimeterHitCollection> m_caloHits{"DRcalo3dHits", Gaudi::DataHandle::Writer, this};
   DataHandle<edm4hep::SparseVectorCollection> m_postprocTime{"DRpostprocTime", Gaudi::DataHandle::Writer, this};
 
   Gaudi::Property<std::string> m_readoutName{this, "readoutName", "DRcaloSiPMreadout", "readout name of DRcalo"};
@@ -47,8 +49,8 @@ private:
   Gaudi::Property<int> m_thres{this, "threshold", static_cast<int>( 1.5 * 10. ), "Integration threshold"};
   Gaudi::Property<int> m_nbins{this, "nbins", 900, "number of bins for FFT"};
 
-  float m_scintSpeed = 174.2; // 173.5; // 158.8 with raw time structure
-  float m_cherenSpeed = 189.5;
+  Gaudi::Property<double> m_scintSpeed{this, "scintSpeed", 174.2, "effective scintillation photon speed in mm/ns"}; // candidate_1 173.5 // candidate_2 158.8
+  Gaudi::Property<double> m_cherenSpeed{this, "cherenSpeed", 189.5, "effective Cherenkov photon speed in mm/ns"};
 };
 
 #endif
--- DRreco/src/DRcalib3D.cpp
+++ DRreco/src/DRcalib3D.cpp
@@ -39,7 +39,7 @@ StatusCode DRcalib3D::execute() {
   const edm4hep::RawCalorimeterHitCollection* digiHits = m_digiHits.get();
   const edm4hep::CalorimeterHitCollection* hits2d = m_2dHits.get();
   const edm4hep::SparseVectorCollection* waveforms = m_waveforms.get();
-  // edm4hep::CalorimeterHitCollection* caloHits = m_caloHits.createAndPut();
+  edm4hep::CalorimeterHitCollection* caloHits = m_caloHits.createAndPut();
   edm4hep::SparseVectorCollection* postprocTimes = m_postprocTime.createAndPut();
 
   for (unsigned int idx = 0; idx < hits2d->size(); idx++) {
@@ -55,6 +55,26 @@ StatusCode DRcalib3D::execute() {
       return StatusCode::FAILURE;
     }
 
+    // set segmentation parameter
+    auto cID = static_cast<dd4hep::DDSegmentation::CellID>( hit2d.getCellID() );
+    int numEta = pSeg->numEta(cID);
+    int numPhi = pSeg->numPhi(cID);
+    pParamBase = pSeg->setParamBase(numEta);
+
+    // estimate fiber geometry
+    auto position = hit2d.getPosition();
+    auto towerPos = pParamBase->GetTowerPos(numPhi);
+    auto waferPos = pParamBase->GetSipmLayerPos(numPhi);
+    dd4hep::Position sipmPos(position.x,position.y,position.z); // type cast to dd4hep::Position
+
+    auto fiberDir = waferPos - towerPos; // outward direction
+    auto fiberUnit = fiberDir.Unit();
+
+    double speed = pSeg->IsCerenkov(cID) ? m_cherenSpeed.value() : m_scintSpeed.value();
+    float effVelocity = speed*dd4hep::millimeter/dd4hep::nanosecond;
+    float invVminusInvC = 1./effVelocity - 1./dd4hep::c_light;
+
+    // create a histogram to do FFT and fill it
     std::unique_ptr<TH1D> waveHist = std::make_unique<TH1D>("waveHist","waveHist",m_nbins,m_gateStart,m_gateStart+m_gateL);
 
     for (unsigned int bin = 0; bin < waveform.centers_size(); bin++) {
@@ -62,7 +82,9 @@ StatusCode DRcalib3D::execute() {
       waveHist->Fill(timeBin,waveform.getContents(bin));
     }
 
+    // Fast Fourier Transform (Z-transform)
     auto* waveProcessed = processFFT(waveHist.get()); // need to delete manually
+    double integral = waveProcessed->Integral();
 
     auto postprocTime = postprocTimes->create();
 
@@ -74,6 +96,8 @@ StatusCode DRcalib3D::execute() {
 
       postprocTime.addToContents( con );
       postprocTime.addToCenters( cen );
+
+      addToTimeStruct(caloHits, hit2d, sipmPos, fiberUnit, invVminusInvC, integral, con, cen);
     }
 
     delete waveProcessed;
@@ -148,122 +172,21 @@ TH1* DRcalib3D::processFFT(TH1* waveHist) {
   return zAns;
 }
 
-// void DRcalib3D::addToTimeStruct(edm4hep::DRrecoCalorimeterHit& drHit, const edm4hep::DRSimCalorimeterHit& input, const float calib) {
-//   auto cID = static_cast<dd4hep::DDSegmentation::CellID>(input.getEdm4hepHit().getCellID());
-//   int numPhi = pSeg->numPhi( cID );
-//   auto towerPos = pParamBase->GetTowerPos(numPhi);
-//   auto waferPos = pParamBase->GetSipmLayerPos(numPhi);
-//   auto sipmPos_ = pSeg->position(cID);
-//   dd4hep::Position sipmPos(sipmPos_.x(),sipmPos_.y(),sipmPos_.z()); // type cast from dd4hep::DDSegmentation::Vector3D to dd4heo::Position
-//
-//   auto fiberDir = waferPos - towerPos; // outward direction
-//   auto fiberUnit = fiberDir.Unit();
-//
-//   if (pSeg->IsCerenkov(cID)) processCheren(drHit,input,calib,sipmPos,fiberUnit);
-//   else processScint(drHit,input,calib,sipmPos,fiberUnit);
-// }
-//
-// void DRcalib3D::processCheren(edm4hep::DRrecoCalorimeterHit& drHit, const edm4hep::DRSimCalorimeterHit& input,
-//                                    const float calib, const dd4hep::Position& sipmPos, const dd4hep::Position& fiberUnit) {
-//   auto& rawhit = input.getEdm4hepHit();
-//
-//   if (rawhit.getAmplitude() < m_minCheren) {
-//     drHit.setValidTimeStruct(0); // too few statistics
-//
-//     return;
-//   }
-//
-//   float effVelocity = m_cherenSpeed*dd4hep::millimeter/dd4hep::nanosecond;
-//   float invVminusInvC = 1./effVelocity - 1./dd4hep::c_light;
-//
-//   for (unsigned int bin = 0; bin < input.timeStruct_size(); bin++) {
-//     float edep = static_cast<float>( input.getTimeStruct(bin) )/calib;
-//     float timeBin = (input.getTimeBegin(bin)+input.getTimeEnd(bin))*dd4hep::nanosecond/2.; // WARNING caution for unit!
-//
-//     float numerator = timeBin - std::sqrt(sipmPos.Mag2())/dd4hep::c_light;
-//     auto pos = sipmPos - ( numerator/invVminusInvC )*fiberUnit;
-//
-//     edm4hep::Vector3f posEdm(pos.x() * CLHEP::millimeter/dd4hep::millimeter,
-//                              pos.y() * CLHEP::millimeter/dd4hep::millimeter,
-//                              pos.z() * CLHEP::millimeter/dd4hep::millimeter);
-//
-//     drHit.addToTimeStruct( edep );
-//     drHit.addToPosition( posEdm );
-//   }
-//
-//   drHit.setValidTimeStruct(2); // ok
-// }
-//
-// void DRcalib3D::processScint(edm4hep::DRrecoCalorimeterHit& drHit, const edm4hep::DRSimCalorimeterHit& input,
-//                                   const float calib, const dd4hep::Position& sipmPos, const dd4hep::Position& fiberUnit) {
-//   auto& rawhit = input.getEdm4hepHit();
-//
-//   if (rawhit.getAmplitude() < m_minScint) {
-//     drHit.setValidTimeStruct(0); // too few statistics
-//
-//     return;
-//   }
-//
-//   float effVelocity = m_scintSpeed*dd4hep::millimeter/dd4hep::nanosecond;
-//   float invVminusInvC = 1./effVelocity - 1./dd4hep::c_light;
-//
-//   float totEdep = static_cast<float>(rawhit.getAmplitude())/calib;
-//
-//   double rangeUpper_wide = m_wideFactor*(m_fitRangeUpper-m_fitRangeLower)+m_fitRangeLower;
-//   std::unique_ptr<TH1D> timeHist = std::make_unique<TH1D>("timeHist","timeHist",m_nbins,m_fitRangeLower,m_fitRangeUpper);
-//   std::unique_ptr<TH1D> timeHist_wide = std::make_unique<TH1D>("timeHist_wide","timeHist_wide",m_nbins,m_fitRangeLower,rangeUpper_wide);
-//
-//   for (unsigned int bin = 0; bin < input.timeStruct_size(); bin++) {
-//     float content = input.getTimeStruct(bin);
-//     float timeBin = (input.getTimeBegin(bin)+input.getTimeEnd(bin))/2.; // in [ns]
-//
-//     int idxBin_narrow = timeHist->GetXaxis()->FindFixBin(timeBin);
-//     int idxBin_wide = timeHist_wide->GetXaxis()->FindFixBin(timeBin);
-//
-//     timeHist->AddBinContent(idxBin_narrow,content);
-//     timeHist_wide->AddBinContent(idxBin_wide,content);
-//   }
-//
-//   std::vector<double> srcMarkov(m_nbins,0.);
-//   for (int bin = 1; bin <= timeHist->GetNbinsX(); bin++)
-//     srcMarkov[bin-1] = timeHist->GetBinContent(bin);
-//
-//   for (int bin = 1; bin <= timeHist->GetNbinsX(); bin++)
-//     timeHist->SetBinContent(bin,srcMarkov[bin-1]);
-//
-//   //
-//
-//   double targetsum = processThreshold(targetMax,target_preprocess,target_postprocess); // cut threshold once more
-//
-//   if ( targetsum <= 0. ) {
-//     drHit.setValidTimeStruct(0); // too few statistics
-//
-//     return;
-//   }
-//
-//   for (auto ts = target_postprocess.begin(); ts != target_postprocess.end(); ++ts) {
-//     float numerator = ts->first - std::sqrt(sipmPos.Mag2())/dd4hep::c_light;
-//     auto pos = sipmPos - ( numerator/invVminusInvC )*fiberUnit;
-//
-//     edm4hep::Vector3f posEdm(pos.x() * CLHEP::millimeter/dd4hep::millimeter,
-//                              pos.y() * CLHEP::millimeter/dd4hep::millimeter,
-//                              pos.z() * CLHEP::millimeter/dd4hep::millimeter);
-//
-//     drHit.addToTimeStruct( (ts->second)*totEdep/targetsum );
-//     drHit.addToPosition( posEdm );
-//   }
-//
-//   drHit.setValidTimeStruct(2);
-// }
-//
-// bool DRcalib3D::pruneTail(const dd4hep::Position& pos, const dd4hep::Position& fiberUnit) {
-//   double innerProduct = pos.Dot(fiberUnit);
-//   return innerProduct > pParamBase->GetCurrentInnerR() - m_pruneTolerance*dd4hep::millimeter;
-// }
-//
-// edm4hep::Vector3f DRcalib3D::getPosition(dd4hep::DDSegmentation::CellID& cID) {
-//   auto globalPos = pSeg->position( cID );
-//   return { static_cast<float>( globalPos.x() * CLHEP::millimeter/dd4hep::millimeter ),
-//            static_cast<float>( globalPos.y() * CLHEP::millimeter/dd4hep::millimeter ),
-//            static_cast<float>( globalPos.z() * CLHEP::millimeter/dd4hep::millimeter ) };
-// }
+void DRcalib3D::addToTimeStruct(edm4hep::CalorimeterHitCollection* caloHits, const edm4hep::CalorimeterHit& hit2d,
+                                const dd4hep::Position& sipmPos, const dd4hep::Position& fiberUnit,
+                                double invVminusInvC, double integral, double con, double cen) {
+  double energy = hit2d.getEnergy()*con/integral;
+  double timeBin = cen*dd4hep::nanosecond;
+  double numerator = timeBin - std::sqrt(sipmPos.Mag2())/dd4hep::c_light;
+  dd4hep::Position pos = sipmPos - ( numerator/invVminusInvC )*fiberUnit;
+  edm4hep::Vector3f posEdm(pos.x() * CLHEP::millimeter/dd4hep::millimeter,
+                           pos.y() * CLHEP::millimeter/dd4hep::millimeter,
+                           pos.z() * CLHEP::millimeter/dd4hep::millimeter);
+
+  auto caloHit = caloHits->create();
+  caloHit.setPosition( posEdm );
+  caloHit.setCellID( hit2d.getCellID() );
+  caloHit.setTime( hit2d.getTime() );
+  caloHit.setType( hit2d.getType() );
+  caloHit.setEnergy( energy );
+}
