f26dcde4b2a7d6827a8124dab3750a6b5a05dd51
jmcarcell <jmcarcell@users.noreply.github.com>
2024-07-29 10:43:58 +0200

    Remove unrelated formatting changes

--- DRdigi/test/runDigi.py
+++ DRdigi/test/runDigi.py
@@ -2,37 +2,25 @@ from Gaudi.Configuration import *
 from Configurables import ApplicationMgr
 
 from Configurables import k4DataSvc
-
 dataservice = k4DataSvc("EventDataSvc", input="sim.root")
 
 from Configurables import PodioInput
-
-podioinput = PodioInput(
-    "PodioInput",
-    collections=[
-        "RawTimeStructs",
-        "RawCalorimeterHits",
-        "SimCalorimeterHits",
-        "Sim3dCalorimeterHits",
-        "RawWavlenStructs",
-        "GenParticles",
-        "Leakages",
-    ],
-    OutputLevel=DEBUG,
-)
+podioinput = PodioInput("PodioInput", collections = ["RawTimeStructs", "RawCalorimeterHits", "SimCalorimeterHits", "Sim3dCalorimeterHits", "RawWavlenStructs", "GenParticles", "Leakages"], OutputLevel = DEBUG)
 
 from Configurables import DigiSiPM
-
 digi = DigiSiPM("DigiSiPM", OutputLevel=DEBUG)
 
 from Configurables import PodioOutput
-
-podiooutput = PodioOutput("PodioOutput", filename="digi.root", OutputLevel=DEBUG)
+podiooutput = PodioOutput("PodioOutput", filename = "digi.root", OutputLevel = DEBUG)
 podiooutput.outputCommands = ["keep *"]
 
 ApplicationMgr(
-    TopAlg=[podioinput, digi, podiooutput],
-    EvtSel="NONE",
-    EvtMax=10,
-    ExtSvc=[dataservice],
+    TopAlg = [
+        podioinput,
+        digi,
+        podiooutput
+    ],
+    EvtSel = 'NONE',
+    EvtMax = 10,
+    ExtSvc = [dataservice]
 )
--- DRsim/DRsimG4Components/test/runDRsim.py
+++ DRsim/DRsimG4Components/test/runDRsim.py
@@ -3,110 +3,92 @@ from Configurables import ApplicationMgr
 from GaudiKernel import SystemOfUnits as units
 
 from Configurables import k4DataSvc
-
 dataservice = k4DataSvc("EventDataSvc")
 
 from Configurables import GenAlg, MomentumRangeParticleGun
-
-pgun = MomentumRangeParticleGun(
-    "PGun",
-    PdgCodes=[11],  # electron
-    MomentumMin=20.0 * units.GeV,  # GeV
-    MomentumMax=20.0 * units.GeV,  # GeV
-    ThetaMin=1.5335,  # rad
-    ThetaMax=1.5335,  # rad
-    PhiMin=0.01745,  # rad
-    PhiMax=0.01745,  # rad
+pgun = MomentumRangeParticleGun("PGun",
+  PdgCodes=[11], # electron
+  MomentumMin = 20.*units.GeV, # GeV
+  MomentumMax = 20.*units.GeV, # GeV
+  ThetaMin = 1.5335, # rad
+  ThetaMax = 1.5335, # rad
+  PhiMin = 0.01745, # rad
+  PhiMax = 0.01745 # rad
 )
 
 from Configurables import FlatSmearVertex
-
-smearTool = FlatSmearVertex(
-    "VertexSmearingTool",
-    yVertexMin=-36.42,  # mm
-    yVertexMax=-26.42,  # mm
-    zVertexMin=-52.135,  # mm
-    zVertexMax=-42.135,  # mm
-    beamDirection=0,  # 1, 0, -1
+smearTool = FlatSmearVertex("VertexSmearingTool",
+  yVertexMin = -36.42, # mm
+  yVertexMax = -26.42, # mm
+  zVertexMin = -52.135, # mm
+  zVertexMax = -42.135, # mm
+  beamDirection = 0 # 1, 0, -1
 )
 
 from Configurables import HepMCToEDMConverter
-
 hepmc2edm = HepMCToEDMConverter("Converter")
 
 gen = GenAlg("ParticleGun", SignalProvider=pgun, VertexSmearingTool=smearTool)
 
 from Configurables import GeoSvc
-
-geoservice = GeoSvc("GeoSvc", detectors=["file:share/compact/DRcalo.xml"])
-
-from Configurables import (
-    SimG4Svc,
-    SimG4FastSimPhysicsList,
-    SimG4FastSimOpFiberRegion,
-    SimG4OpticalPhysicsList,
+geoservice = GeoSvc(
+  "GeoSvc",
+  detectors = [
+    'file:share/compact/DRcalo.xml'
+  ]
 )
 
+from Configurables import SimG4Svc, SimG4FastSimPhysicsList, SimG4FastSimOpFiberRegion, SimG4OpticalPhysicsList
 regionTool = SimG4FastSimOpFiberRegion("fastfiber")
-opticalPhysicsTool = SimG4OpticalPhysicsList(
-    "opticalPhysics", fullphysics="SimG4FtfpBert"
-)
+opticalPhysicsTool = SimG4OpticalPhysicsList("opticalPhysics", fullphysics="SimG4FtfpBert")
 physicslistTool = SimG4FastSimPhysicsList("Physics", fullphysics=opticalPhysicsTool)
 
 from Configurables import SimG4DRcaloActions
-
 actionTool = SimG4DRcaloActions("SimG4DRcaloActions")
 
 # Name of the tool in GAUDI is "XX/YY" where XX is the tool class name and YY is the given name
-geantservice = SimG4Svc(
-    "SimG4Svc",
-    physicslist=physicslistTool,
-    regions=["SimG4FastSimOpFiberRegion/fastfiber"],
-    actions=actionTool,
+geantservice = SimG4Svc("SimG4Svc",
+  physicslist = physicslistTool,
+  regions = ["SimG4FastSimOpFiberRegion/fastfiber"],
+  actions = actionTool
 )
 
 from Configurables import SimG4Alg, SimG4PrimariesFromEdmTool
-
 # next, create the G4 algorithm, giving the list of names of tools ("XX/YY")
 edmConverter = SimG4PrimariesFromEdmTool("EdmConverter")
 
 from Configurables import SimG4SaveDRcaloHits, SimG4SaveDRcaloMCTruth
+saveDRcaloTool = SimG4SaveDRcaloHits("saveDRcaloTool", readoutNames = ["DRcaloSiPMreadout"])
+saveMCTruthTool = SimG4SaveDRcaloMCTruth("saveMCTruthTool") # need SimG4DRcaloActions
 
-saveDRcaloTool = SimG4SaveDRcaloHits(
-    "saveDRcaloTool", readoutNames=["DRcaloSiPMreadout"]
-)
-saveMCTruthTool = SimG4SaveDRcaloMCTruth("saveMCTruthTool")  # need SimG4DRcaloActions
-
-geantsim = SimG4Alg(
-    "SimG4Alg",
-    outputs=[
-        "SimG4SaveDRcaloHits/saveDRcaloTool",
-        "SimG4SaveDRcaloMCTruth/saveMCTruthTool",
-    ],
-    eventProvider=edmConverter,
+geantsim = SimG4Alg("SimG4Alg",
+  outputs = [
+    "SimG4SaveDRcaloHits/saveDRcaloTool",
+    "SimG4SaveDRcaloMCTruth/saveMCTruthTool"
+  ],
+  eventProvider = edmConverter
 )
 
 from Configurables import PodioOutput
-
-podiooutput = PodioOutput("PodioOutput", filename="sim.root")
+podiooutput = PodioOutput("PodioOutput", filename = "sim.root")
 podiooutput.outputCommands = ["keep *"]
 
 from Configurables import RndmGenSvc, HepRndm__Engine_CLHEP__RanluxEngine_
-
-rndmEngine = HepRndm__Engine_CLHEP__RanluxEngine_(
-    "RndmGenSvc.Engine",
-    SetSingleton=True,
-    UseTable=True,
-    Column=0,  # 0 or 1
-    Row=123,  # 0 to 214
+rndmEngine = HepRndm__Engine_CLHEP__RanluxEngine_("RndmGenSvc.Engine",
+  SetSingleton = True,
+  UseTable = True,
+  Column = 0, # 0 or 1
+  Row = 123 # 0 to 214
 )
 
-rndmGenSvc = RndmGenSvc("RndmGenSvc", Engine=rndmEngine.name())
+rndmGenSvc = RndmGenSvc("RndmGenSvc",
+  Engine = rndmEngine.name()
+)
 
 ApplicationMgr(
-    TopAlg=[gen, hepmc2edm, geantsim, podiooutput],
-    EvtSel="NONE",
-    EvtMax=10,
-    # order is important, as GeoSvc is needed by SimG4Svc
-    ExtSvc=[rndmEngine, rndmGenSvc, dataservice, geoservice, geantservice],
+  TopAlg = [gen, hepmc2edm, geantsim, podiooutput],
+  EvtSel = 'NONE',
+  EvtMax = 10,
+  # order is important, as GeoSvc is needed by SimG4Svc
+  ExtSvc = [rndmEngine, rndmGenSvc, dataservice, geoservice, geantservice]
 )
